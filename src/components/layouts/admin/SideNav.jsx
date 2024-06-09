9957c55ce95ef47fb2b089bfd8e9f979c5d5d3f8
Tudzy <narongsak5az@gmail.com>
2024-06-09 20:16:55 +0700

    .

--- src/components/layouts/admin/SideNav.jsx
+++ src/components/layouts/admin/SideNav.jsx
@@ -8,7 +8,7 @@ const SideNav = () => {
                 {/* Brand Logo */}
                 <a href="index3.html" className="brand-link">
                     <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" className="brand-image img-circle elevation-3" style={{ opacity: '.8' }} />
-                    <span className="brand-text font-weight-light">syst</span>
+                    <span className="brand-text font-weight-light">system name</span>
                 </a>
                 {/* Sidebar */}
                 <div className="sidebar">
